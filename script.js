16408f17a505ba678811ae46b629e685f909b8ce
Denis Trunin <trunden02@gmail.com>
2023-07-03 13:47:14 +0300

    commit 12

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+$(document).ready(function () {
+	$(".video-gallery").magnificPopup({
+		delegate: "a",
+		type: "iframe",
+		gallery: {
+			enabled: true
+		}
+	});
+});
+
 const months = [
 	"Январь",
 	"Февраль",
